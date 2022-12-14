de7a1c32b708e2c2bc838bbd31b186911a06209b
Diana Diaz <93556710+WinterLanterns@users.noreply.github.com>
2022-12-14 12:03:37 -0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import { Routes, Route } from "react-router-dom";
 import styled from "styled-components";
-
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
